db92c1dbc4afa76da4c445817d4f771045df65a7
John Chen <john.hk.chen@gmail.com>
2023-12-25 09:44:26 -0800

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',      
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/chicken-business.css',
+			],
+			social: {
+				github: 'https://blah28722.github.io/',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
